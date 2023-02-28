6c45785a6ef8bccb598b0f3b82f15d7fba258d7a
Jay <jayprest@hotmail.com>
2023-02-28 10:15:57 +0000

    Drills Completed

--- ruby_chapter_3_challenges/drills/lib/1_arrays_and_hashes.rb
+++ ruby_chapter_3_challenges/drills/lib/1_arrays_and_hashes.rb
@@ -8,17 +8,35 @@
 # takes one array as an arg
 # returns the fourth element
 
+def fourth_element(array)
+    array[3]
+end
+
 # average
 # takes an array of numbers
 # returns the average (the mean)
+
+def average(array)
+    array.sum / array.size
+end
 
 # lowest_squared
 # takes an array of numbers
 # returns the lowest number squared
 
+def lowest_squared(array)
+    array.map! {|num| num ** 2}
+    return array.min
+end
+
 # highest_squared
 # takes an array of numbers
 # returns the highest number squared
+
+def highest_squared(array)
+    array.map! {|num| num ** 2}
+    return array.max
+end
 
 # Hashes I
 
@@ -26,13 +44,25 @@
 # takes a hash where the values are numbers
 # returns the total of all the values
 
+def values_summed(hash)
+    hash.values.sum
+end
+
 # first_two_keys
 # takes a hash
 # returns the first two keys
 
+def first_two_keys(hash)
+    hash.keys[..1]
+end
+
 # first_two_values
 # takes a hash
 # returns the first two values
+
+def first_two_values(hash)
+    hash.values[..1]
+end
 
 # Arrays II
 
@@ -40,9 +70,17 @@
 # takes one array as an arg
 # returns only elements starting with 'a'
 
+def starts_with_a(array)
+    array.select{|x| x.match?(/^a/)}
+end
+
 # starts_with_a_vowel
 # takes an array as an arg
 # returns only the elements that start with a vowel
+
+def starts_with_a_vowel(array)
+    array.select{|x| x.match?(/^a/) || x.match?(/^e/) ||  x.match?(/^i/) || x.match?(/^o/) || x.match?(/^u/)}
+end
 
 # reverse_each_element
 # takes an array as an arg
@@ -50,37 +88,81 @@
 # E.g. when the arg is ['one', 'two']
 # it returns ['eno', 'owt']
 
+def reverse_each_element(array)
+    array.select {|x| x.reverse!}
+end
+
 # sort_by_last_letter
 # takes one array as an arg
 # returns the array, sorted by the last letter
+
+def sort_by_last_letter(array)
+    array.sort_by {|x| x[-1]}
+end
 
 # greater_than_5
 # takes one array as an arg
 # returns only numbers greater than 5
 
+def greater_than_5(array)
+    array.select{|x| x > 5}
+end
+
 # greater_than
 # takes one array and one number as args
 # returns only the elements that are greater than the number provided
+
+def greater_than(array, num)
+    array.select{|x| x > num}
+end
 
 # less_than
 # takes one array and one number as args
 # returns only the elements that are less than the number provided
 
+def less_than(array, num)
+    array.select{|x| x < num}
+end
+
 # words_shorter_than
 # takes one array and one number as args
 # returns only the elements than have fewer characters than the number provided
+
+def words_shorter_than(array, num)
+    array.select { |x| x.length < num }
+end
 
 # all_above?
 # takes an array and a number as args
 # returns true if all elements are greater than the number provided
 
+def all_above?(array, num)
+    if array.all?{|x| x > num}
+        true
+    else
+        false
+    end
+end
+
 # all_below?
 # takes an array and a number as args
 # returns true if all elements are less than the number provided
 
+def all_below?(array, num)
+    if array.all?{|x| x < num}
+        true
+    else
+        false
+    end
+end
+
 # multiply_each_by
 # takes one array and one number as args
 # returns a new array with each element multiplied by the number provided
+
+def multiply_each_by(array, num)
+    array.map!{|x| x * num }
+end
 
 # Hashes II
 
@@ -88,18 +170,40 @@
 # takes a hash and a number as args
 # returns key value pairs where the value is less than the number provided
 
+def where_value_below(hash, num)
+    hash.select{|key, value| value < num}
+end
+
 # where_key_starts_with
 # takes a hash and a letter as args
 # returns key value pairs where the key starts with the letter provided
+
+def where_key_starts_with(hash, char)
+    hash.select{|key, value| key.match?(/^#{char}/)}
+end
 
 # add_key_value_pair
 # takes a hash, a key and a value as args
 # returns a the hash with the new key and value added
 
+def add_key_value_pair(hash, key, value)
+    hash[key] = value
+    return hash
+end
+
 # sort_by_key
 # takes a hash
 # sorts key-value pairs by key
 
+def sort_by_key(hash)
+    hash.sort.to_h
+    return hash
+end
+
 # sort_by_value
 # takes a hash
 # sorts key-value pairs by value
+
+def sort_by_value(hash)
+    hash.sort_by {|key, value| value}.to_h
+end
--- ruby_chapter_3_challenges/drills/lib/2_classes.rb
+++ ruby_chapter_3_challenges/drills/lib/2_classes.rb
@@ -45,12 +45,39 @@
 # Takes one string as an arg (a name)
 # Returns 'Goodmorning, Alex', if the arg is 'Alex'
 
+class Greeter
+  def hello(name)
+    "Hello, #{name}"
+  end
+  def goodbye(name)
+    "Goodbye, #{name}"
+  end
+  def goodnight(name)
+    "Goodnight, #{name}"
+  end
+  def goodmorning(name)
+    "Goodmorning, #{name}"
+  end
+end
+
 # Basket
 # add
 # takes one argument of any type
 # adds it to the list of items
 # items
 # returns everything that has been added to the basket
+
+class Basket
+  def initialize
+    @array = []
+  end
+  def add(arg)
+    @array << arg
+  end
+  def items
+    @array
+  end
+end
 
 # Basket2
 # add
@@ -60,6 +87,21 @@
 # returns everything that has been added to the basket
 # eg: after having added "carrots", 3
 # contents returns [{"item" => "carrots", "amount" => 3}]
+
+class Basket2
+  def initialize
+    @array = []
+  end
+  def add(string, amount)
+    hash = {}
+    hash["item"] = string
+    hash["amount"] = amount
+    @array.push(hash)
+  end
+  def contents
+    @array
+  end
+end
 
 # Calculator
 # add
@@ -83,6 +125,39 @@
 # eg: after multiplying 4 and 6, the history is 
 # [{"operation" => "multiply", "arguments" => [4, 6], "result" => 24}]
 
+class Calculator
+  def initialize
+    @array = []
+  end
+  def add(num1, num2)
+    x = num1 + num2
+    hash = {"operation" => "add", "arguments" => [num1, num2], "result" => x} 
+    @array.push(hash) 
+    return x 
+  end
+  def multiply(num1, num2)
+    x = num1 * num2
+    hash = {"operation" => "multiply", "arguments" => [num1, num2], "result" => x}
+    @array.push(hash)
+    return x
+  end
+  def subtract(num1, num2)
+    x = num1 - num2
+    hash = {"operation" => "subtract", "arguments" => [num1, num2], "result" => x}
+    @array.push(hash)
+    return x 
+  end
+  def divide(num1, num2)
+    x = num1 / num2
+    hash = {"operation" => "divide", "arguments" => [num1, num2], "result" => x}
+    @array.push(hash)
+    return x
+  end
+  def history
+    @array
+  end
+end
+
 # Cohort
 # add_student
 # takes one hash, representing a student, as an arg.
@@ -94,6 +169,21 @@
 # takes one string, the name of an employer, as an arg.
 # E.g. 'NASA'
 # returns only the students who work for that employer
+
+class Cohort
+  def initialize
+    @array = []
+  end
+  def add_student(student)
+    @array.push(student)
+  end
+  def students
+    return @array
+  end
+  def employed_by(employer)
+    @array.filter {|key, value| key["employer"] == employer}
+  end
+end
 
 # Person
 # is initialized with a complex hash, for example...
@@ -110,6 +200,30 @@
   ]
 }
 
+class Person
+  def initialize(person)
+    @person = person
+  end
+  def work_address
+    work_details = @person["addresses"].select {|key| key["name"] == "work"}[0]
+    "#{work_details["building"]} #{work_details["street"]}"
+  end
+  def home_address
+    home_details = @person["addresses"].select {|key| key["name"] == "home"}[0]
+    "#{home_details["building"]} #{home_details["street"]}"
+  end
+  def pets
+    petCount = @person["pets"].count
+    name = @person["name"]
+    pets = @person["pets"]
+    animalCollection = "#{name}" + " has " + "#{petCount}" + " pets\n"
+    pets.each do |key, value|
+      animalCollection << "- a " + key["animal"] + " called " + key["name"]+"\n"
+    end
+    return animalCollection
+  end
+end
+
 # work_address
 # takes no args
 # returns the work address in a nice format
@@ -122,9 +236,8 @@
 # takes no args
 # returns a nice summary of the person's pets
 
-# E.g.
+# E.g
 # Alex has 3 pets
 # - a cat called Arthur
 # - a dog called Judith
 # - a goldfish called Gwen
-#
